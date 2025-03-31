bed45a926e6881917c4dbb92e4ac5e7dd3e73a7d
Derek <derekl07@bu.edu>
2025-03-31 16:52:01 -0400

    3/31 meeting

--- truss_project.m
+++ truss_project.m
@@ -3,7 +3,8 @@ Truss Project Preliminary Design
 
 [num_joints, num_members] = size(C);
 
-v = zeros(num_members, 2);
+Vx = zeros(num_members, 1);
+Vy = zeros(num_members, 1);
 r = zeros(num_members, 1);
 
 %For each member, find its 2 connections
@@ -12,12 +13,14 @@ for m = 1:num_members
     
     joint1 = joints(connected_joints(1), :);
     joint2 = joints(connected_joints(2), :);
-    
-    v(m, :) = joint2 - joint1;
-    r(m) = norm(v(m, :));
+    v = joint2-joint1;
+    r = norm(v);
+
+    Vx(m) = v(1)/r(m);
+    Vy(m) = v(2)/r(m);
 end
-% v will be vector containing the member's locations
 % r will be array containing the member's lengths
+% Vx and Vy will be the unit vectors in the direction later-earlier
 
 
 
